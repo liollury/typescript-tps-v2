5faa8a3d14ef50e6938cb4cfde0e67467b45d57b
Olivier LE LAY <liollury@gmail.com>
2021-10-08 15:07:19 +0200

    fix: remove function

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,22 +1,12 @@
 let library = [];
 
-function createBook(name, price, ISBN) {
-    return {
-        name,
-        price,
-        ISBN,
-        read: false
-    };
-}
+library.push({name: 'De soleil et de sang', price: 8.2, ISBN: '2253079405'});
+library.push({name: 'Le douzième chapitre', price: 7.9, ISBN: '2253079654'});
+library.push({name: 'Au bout de la nuit', price: 8.4, ISBN: 2259874405});
 
-
-library.push(createBook('De soleil et de sang', 8.2, '2253079405'));
-library.push(createBook('Le douzième chapitre', 7.9, '2253079654'));
-library.push(createBook('Au bout de la nuit', 8.4, 2259874405));
-
-const spiderman1 = createBook('Spider-man: un nouveau départ', '9.95', '08742875363');
-const spiderman2 = createBook('Spider-man: Vénon', 9.95, '08742875345');
-const hulk = createBook('Hulk: Briseur de monde', 18, '0347938560');
+const spiderman1 = {name: 'Spider-man: un nouveau départ', price: '9.95', ISBN: '08742875363'};
+const spiderman2 = {name: 'Spider-man: Vénon', price: 9.95, ISBN: '08742875345'};
+const hulk = {name: 'Hulk: Briseur de monde', price: 18, ISBN: '0347938560'};
 library.push(spiderman1, spiderman2, hulk);
 
 const cheapestBooks = library.filter((book) => parseFloat(book.price) < 10);
